372e8e309b7e71032485e67ff3d12d33329de699
FelipeFaura <felipefaura@gmail.com>
2019-12-23 10:48:30 +0100

    Bug found with the previous commit in header component. Actual WIP Created access to JSON information in firebase through Https Created interface for TeamMembers information

---
+++ src/app/interfaces/team.ts
@@ -0,0 +1,7 @@
+export interface TeamMember {
+  frase: string;
+  nombre: string;
+  subtitulo: string;
+  twitter: string;
+  url: string;
+}
--- src/app/services/info-page.service.ts
+++ src/app/services/info-page.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Info } from '../interfaces/info-page';
+import { TeamMember } from '../interfaces/team';
 
 @Injectable({
   providedIn: 'root'
@@ -11,16 +12,30 @@ export class InfoPageService {
 
   info: Info;
   charged = false;
+  teamMember: TeamMember[];
 
   constructor(private http: HttpClient) {
+    this.getInfo();
+    //this.getTeam();
 
-    console.log('Test de servicio');
+  }
+
+  private getInfo() {
     this.http.get('assets/data/data-page.json')
       .subscribe((resp: Info) => {
         this.charged = true;
         this.info = resp;
         console.log(this.info);
+        console.log(this.info.titulo);
+      });
+  }
 
+  private getTeam() {
+    this.http.get('https://portfolio-55393.firebaseio.com/equipo/0.json')
+      .subscribe((resp: any[]) => {
+        this.charged = true;
+        this.teamMember = resp;
+        console.log(this.teamMember);
       });
   }
 }
